7dffc4f4c6a93814216574f14d37f72eda31048e
Vinicius <viniciusfod@gmail.com>
2024-01-18 16:59:00 -0300

    fix: login next auth

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,16 +1,8 @@
 import AuthWithGoogle from "@/components/AuthWithGoogle";
-import { auth } from "@/lib/auth"
 
 export default function Login() {
-  const session = auth()
-
-  if (session?.user) {
-    console.log(session.user);
-  }
-
   return (
     <div>
-      {session?.user?.email}
       <div>Login</div>
       <AuthWithGoogle />
     </div>
