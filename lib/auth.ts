18be56169ce835c0945f1c698467ecb5395f6a4c
vinicius <viniciusfod@gmail.com>
2024-02-04 01:00:36 -0300

    add svg logo btn microsoft

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AzureADProvider from "next-auth/providers/azure-ad";
+import AzureADProvider from "next-auth/providers/azure-ad"
 import type { NextAuthConfig } from "next-auth";
 import axios from "axios";
 
