e014ef4422dca0be9b049f0d829155c64ba9a970
vinicius <viniciusfod@gmail.com>
2024-06-04 12:42:19 -0300

    vercel

--- next.config.js
+++ next.config.js
@@ -30,4 +30,17 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    } 
 });
