56615dca4b93052f0d5c5a6b0ebd53b523b88b8b
vinicius <viniciusfod@gmail.com>
2024-06-05 13:55:30 -0300

    sw

--- next.config.js
+++ next.config.js
@@ -1,19 +1,35 @@
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} = require("next/constants");
+/** @type {import('next').NextConfig} */
 
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  runtimeCaching: [
+    //cache assets & data from external api
+    {
+      urlPattern: /.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "others", // cache name
+        expiration: {
+          maxEntries: 32, // max cache entries
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+        networkTimeoutSeconds: 20, // fall back to cache if api does not response within 20 seconds
+      },
+    },
+  ],
+});
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = require("@ducanh2912/next-pwa").default({
-      dest: "public",
-    });
-    return withPWA(nextConfig);
-  }
-  return nextConfig;
-};
+module.exports = withPWA({
+    runtimeCaching: true,
+    buildExcludes: [/middleware-manifest.json$/],
+    reactStrictMode: true,
+    swcMinify: true,
+});
