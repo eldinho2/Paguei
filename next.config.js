1c124ca66d80259c0c5c7419585dcb0568f80835
vinicius <viniciusfod@gmail.com>
2024-06-05 14:57:11 -0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,35 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  runtimeCaching: [
-    //cache assets & data from external api
-    {
-      urlPattern: /.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "others", // cache name
-        expiration: {
-          maxEntries: 32, // max cache entries
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        networkTimeoutSeconds: 20, // fall back to cache if api does not response within 20 seconds
-      },
-    },
-  ],
-});
+const withPWA = require("@ducanh2912/next-pwa").default;
 
 module.exports = withPWA({
-    runtimeCaching: true,
-    buildExcludes: [/middleware-manifest.json$/],
-    reactStrictMode: true,
-    swcMinify: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }
 });
